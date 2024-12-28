bc5467f0448aeb0cd54b1a4d96063b4f6f6c2fd6
Edir P Santos <fennder@gmail.com>
2024-12-28 17:07:03 -0300

    Commit first project Business Around Me

---
+++ src/main/java/br/tec/abrindoportas/BusinessAroundMe/BusinessAroundMeApplication.java
@@ -0,0 +1,13 @@
+package br.tec.abrindoportas.BusinessAroundMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessAroundMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessAroundMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/tec/abrindoportas/BusinessAroundMe/BusinessAroundMeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tec.abrindoportas.BusinessAroundMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessAroundMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
